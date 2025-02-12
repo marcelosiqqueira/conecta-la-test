fde1a404141ef85d44e0da7fa49e5639c4eb0011
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2025-02-12 09:21:12 -0300

    feat: create user requests

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+
+    public function wantsJson()
+    {
+        return true;
+    }
+}
